2d4e159d838f4656ae9133460caacc3242628f5f
Paddy O'Hanlon <paddy@paddyohanlon.com>
2021-07-06 15:17:40 +0300

    Attempt cache invalidation

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,11 @@
-var CACHE_NAME = "my-site-cache-v1";
-var urlsToCache = ["/"];
+const version = "V0.01";
+const CACHE_NAME = version + "staticfiles";
+const urlsToCache = ["/"];
 
-self.addEventListener("install", function (event) {
+self.addEventListener("install", (installEvent) => {
   // Perform install steps
-  event.waitUntil(
+  skipWaiting();
+  installEvent.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log("Opened cache");
       return cache.addAll(urlsToCache);
@@ -11,14 +13,34 @@ self.addEventListener("install", function (event) {
   );
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
+addEventListener("activate", (activateEvent) => {
+  activateEvent.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName != staticCacheName) {
+              return caches.delete(cacheName);
+            } // end if
+          }) // end map
+        ); // end return Promise.all
+      }) // end keys then
+      .then(() => {
+        return clients.claim();
+      }) // end then
+  ); // end waitUntil
+}); // end addEventListener
+
+self.addEventListener("fetch", (fetchEvent) => {
+  const request = fetchEvent.request;
+  fetchEvent.respondWith(
+    caches.match(request).then((response) => {
       // Cache hit - return response
       if (response) {
         return response;
       }
-      return fetch(event.request);
+      return fetch(request);
     })
   );
 });
